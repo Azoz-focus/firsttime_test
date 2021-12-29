0bdc5a3c7e307b82a32a7c62d7c01dfabd25ecec
Azoz-focus <azozfocusgt@gmail.com>
2021-12-29 10:44:48 -0800

    testing.py new changes

--- testing.py
+++ testing.py
@@ -1,1 +1,34 @@
-import random
+import os
+import time
+
+b= True
+
+def quiz():
+    c= True
+    while c==True:
+        print("how are you? ")
+        alhamdllilah=input()
+        if alhamdllilah == "alhamdulillah":
+            c=False
+            time.sleep(2)
+            print("you are clever!")
+            time.sleep(5)
+            quit()
+        else:
+            print("try again!")
+            print("\n")
+            time.sleep(2)
+            os.system('cls')
+
+while b==True:
+    print("hello whats your name? ")
+    sync= input()
+    if sync == 'sceptic':
+        b=False
+        quiz()
+    else:
+        print("try again!")
+        print("\n")
+        time.sleep(2)
+        os.system('cls')
+
